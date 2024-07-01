19e65604cbfe0ca0495f2ee7337718562cf57553
José Inácio Bitencourt da Silva <129962133+JoseInacioDS@users.noreply.github.com>
2024-07-01 10:35:29 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+
+const mes = [
+    'janeiro'
+  ];
+  
+  for (let i = 0; i < mes.length; i++) {
+    console.log(`${mes[i]}`);
+    if (mes[i] === 'janeiro') {
+        console.log(`nasci em mês: ${mes[i]}`)
+        break;
+    }
+  }
+  
